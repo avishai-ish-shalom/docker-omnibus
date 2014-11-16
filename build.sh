c86f0eb9a3e4c806714ae6115a2adb69ac00794f
Avishai Ish-Shalom <avishai@fewbytes.com>
2014-11-16 08:48:00 +0200

    change output image repositories

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker pull centos:centos6
-packer build -var base_image=centos:centos6 -var output_repository=omnibus/centos -var output_tag=6 centos-packer.json
+packer build -var base_image=centos:centos6 -var output_repository=omnibus/centos-6 -var output_tag=latest centos-packer.json
 docker pull centos:centos7
-packer build -var base_image=centos:centos7 -var output_repository=omnibus/centos -var output_tag=7 centos-packer.json
+packer build -var base_image=centos:centos7 -var output_repository=omnibus/centos-7 -var output_tag=latest centos-packer.json
 docker pull ubuntu:precise
-packer build -var base_image=ubuntu:precise -var output_repository=omnibus/ubuntu -var output_tag=precise ubuntu-packer.json
+packer build -var base_image=ubuntu:precise -var output_repository=omnibus/ubuntu-precise -var output_tag=latest ubuntu-packer.json
 docker pull debian:wheezy
-packer build -var base_image=debian:wheezy -var output_repository=omnibus/ubuntu -var output_tag=wheezy ubuntu-packer.json
+packer build -var base_image=debian:wheezy -var output_repository=omnibus/debian-wheezy -var output_tag=latest ubuntu-packer.json
