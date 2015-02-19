01969289209a42e29e57441c1fabceb06efa4580
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-02-19 22:54:28 +0200

    fixed tag names

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 docker pull centos:centos6
-packer build -var base_image=centos:centos6 -var output_repository=omnibus/centos-6 -var output_tag=latest centos-packer.json
+packer build -var base_image=centos:centos6 -var output_repository=omnibus/centos:6 -var output_tag=latest centos-packer.json
 docker pull centos:centos7
-packer build -var base_image=centos:centos7 -var output_repository=omnibus/centos-7 -var output_tag=latest centos-packer.json
+packer build -var base_image=centos:centos7 -var output_repository=omnibus/centos:7 -var output_tag=latest centos-packer.json
 docker pull ubuntu:precise
-packer build -var base_image=ubuntu:precise -var output_repository=omnibus/ubuntu-precise -var output_tag=latest ubuntu-packer.json
+packer build -var base_image=ubuntu:precise -var output_repository=omnibus/ubuntu:precise -var output_tag=latest ubuntu-packer.json
+docker pull ubuntu:trusty
+packer build -var base_image=ubuntu:trusty -var output_repository=omnibus/ubuntu:trusty -var output_tag=latest ubuntu-packer.json
 docker pull debian:wheezy
-packer build -var base_image=debian:wheezy -var output_repository=omnibus/debian-wheezy -var output_tag=latest ubuntu-packer.json
+packer build -var base_image=debian:wheezy -var output_repository=omnibus/debian:wheezy -var output_tag=latest ubuntu-packer.json
